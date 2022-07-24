cce0be06bc638596521fbd60f1577c429e787389
Enoc Castro Vargas <enocco70@gmail.com>
2022-07-24 14:38:19 -0600

    load event on modal

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,17 +1,25 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { onSetActiveEvent } from '../store';
 
 
 export const useCalendarStore = () => {
 
-    
+    const dispatch = useDispatch();
     const { events, activeEvent } = useSelector( state => state.calendar );
+
+    const setActiveEvent = ( calendarEvent ) => {
+        dispatch( onSetActiveEvent( calendarEvent ) );
+
+
+    };
 
     return {
         //* Propiedades
         activeEvent,
-        events
+        events,
 
         //* Métodos
+        setActiveEvent,
 
     };
 };
