894fa7c05ac9898853337f61e95595fb6f2d02b6
Enoc Castro Vargas <enocco70@gmail.com>
2022-09-25 16:35:24 -0600

    Mantener el estado de la autenticación

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -13,7 +13,7 @@ export const useAuthStore = () => {
         dispatch( onChecking() );
 
         try {
-            const { data } = await calendarApi.post('/auth', { email, password });
+            const { data } = await calendarApi.post('auth', { email, password });
             localStorage.setItem('token', data.token );
             localStorage.setItem('token-init-date', new Date().getTime() );
             dispatch( onLogin( { name: data.name, uid: data.uid }) );
@@ -30,7 +30,7 @@ export const useAuthStore = () => {
     const startRegister = async({ email, password, name }) => {
         dispatch( onChecking() );
         try {
-            const { data } = await calendarApi.post('/auth/new', { email, password, name });
+            const { data } = await calendarApi.post('auth/new', { email, password, name });
             localStorage.setItem('token', data.token );
             localStorage.setItem('token-init-date', new Date().getTime() );
             dispatch( onLogin( { name: data.name, uid: data.uid }) );
@@ -44,6 +44,23 @@ export const useAuthStore = () => {
 
     };
 
+    const checkAuthToken = async() => {
+        const token = localStorage.getItem('token');
+        if ( !token ) return dispatch( onLogout() );
+
+        try {
+            const { data } = await calendarApi.get('auth/renew');
+            console.log({ data });
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('token-init-date', new Date().getTime() );
+            dispatch( onLogin( { name: data.name, uid: data.uid }) );
+        } catch (error) {
+            localStorage.clear();
+            dispatch( onLogout() );
+        }
+
+    }
+
     return {
         //* Propiedades
         errorMessage,
@@ -51,6 +68,7 @@ export const useAuthStore = () => {
         user,
 
         //* Métodos
+        checkAuthToken,
         startLogin,
         startRegister,
         
