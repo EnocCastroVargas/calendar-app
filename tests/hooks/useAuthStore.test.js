522892b9e5a7e93255c43cab1173730c48bfed5f
Enoc Castro Vargas <enocco70@gmail.com>
2022-09-30 17:25:45 -0600

    startLogin debe de fallar la autenticación

--- tests/hooks/useAuthStore.test.js
+++ tests/hooks/useAuthStore.test.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { act, renderHook } from '@testing-library/react';
+import { act, renderHook, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { useAuthStore } from '../../src/hooks/useAuthStore';
 import { authSlice } from '../../src/store';
@@ -60,6 +60,29 @@ describe('Pruebas en el useAuthStore', () => {
 
         expect( localStorage.getItem('token') ).toEqual( expect.any(String) );
         expect( localStorage.getItem('token-init-date') ).toEqual( expect.any(String) );
+    });
+    
+    test('startLogin debe de fallar la autenticación', async() => {
+        localStorage.clear();
+        const mockStore = getMockStore({ ...notAuthenticatedState });   
+        const { result } = renderHook( () => useAuthStore(), {
+            wrapper: ({ children }) => <Provider store={ mockStore }>{ children }</Provider>
+        });
+        
+        await act( async() => {
+            await result.current.startLogin({ email: 'algo@google.com', password: '123456789' });
+        });
+
+        const { errorMessage, status, user } = result.current;
+        expect(localStorage.getItem('token')).toBe(null);
+        expect({ errorMessage, status, user }).toEqual({
+            errorMessage: 'Credenciales incorrectas',
+            status: 'not-authenticated',
+            user: {}
+        });
+        await waitFor(
+            () => expect( result.current.errorMessage ).toBe( undefined )
+        );
     });
 
 });
