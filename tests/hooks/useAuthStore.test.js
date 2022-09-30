dcbe1ff79490b475878ba061799a4f729feb257c
Enoc Castro Vargas <enocco70@gmail.com>
2022-09-29 18:14:05 -0600

    startLogin debe de realizar el login correctamente

--- tests/hooks/useAuthStore.test.js
+++ tests/hooks/useAuthStore.test.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { useAuthStore } from '../../src/hooks/useAuthStore';
 import { authSlice } from '../../src/store';
-import { initialState } from '../fixtures/authState';
+import { initialState, notAuthenticatedState } from '../fixtures/authState';
+import { testUserCredentials } from '../fixtures/testUser';
 
 
 const getMockStore = ( initialState ) => {
@@ -35,6 +36,30 @@ describe('Pruebas en el useAuthStore', () => {
             startLogout: expect.any(Function),
             startRegister:expect.any(Function),
         });
+    });
+
+    test('startLogin debe de realizar el login correctamente', async() => {
+        // Se limpi por si alguna prueba haya grabado el x-token o
+        // el token-init-date  y nos puede dar un falso positivo
+        localStorage.clear(); 
+        const mockStore = getMockStore({ ...notAuthenticatedState });   
+        const { result } = renderHook( () => useAuthStore(), {
+            wrapper: ({ children }) => <Provider store={ mockStore }>{ children }</Provider>
+        });
+        
+        await act( async() => {
+            await result.current.startLogin( testUserCredentials );
+        });
+
+        const { errorMessage, status, user } = result.current;
+        expect({ errorMessage, status, user }).toEqual({
+            errorMessage: undefined,
+            status: 'authenticated',
+            user: { name: 'Test User', uid: '6335cd909157e9a4cccfb2a6' },
+        });
+
+        expect( localStorage.getItem('token') ).toEqual( expect.any(String) );
+        expect( localStorage.getItem('token-init-date') ).toEqual( expect.any(String) );
     });
 
 });
