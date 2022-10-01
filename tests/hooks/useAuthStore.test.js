e96395a70801cf599855c0d6482b6502b069b9cc
Enoc Castro Vargas <enocco70@gmail.com>
2022-09-30 18:41:26 -0600

    startRegister debe de crear un usuario

--- tests/hooks/useAuthStore.test.js
+++ tests/hooks/useAuthStore.test.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { act, renderHook, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { calendarApi } from '../../src/api';
 import { useAuthStore } from '../../src/hooks/useAuthStore';
 import { authSlice } from '../../src/store';
 import { initialState, notAuthenticatedState } from '../fixtures/authState';
@@ -19,10 +20,11 @@ const getMockStore = ( initialState ) => {
 }
 
 describe('Pruebas en el useAuthStore', () => {
+
+    beforeEach(() => localStorage.clear());
     
     test('debe de regresar los valores por defecto', () => {
         const mockStore = getMockStore({ ...initialState });
-        
         const { result } = renderHook( () => useAuthStore(), {
             wrapper: ({ children }) => <Provider store={ mockStore }>{ children }</Provider>
         });
@@ -41,7 +43,6 @@ describe('Pruebas en el useAuthStore', () => {
     test('startLogin debe de realizar el login correctamente', async() => {
         // Se limpi por si alguna prueba haya grabado el x-token o
         // el token-init-date  y nos puede dar un falso positivo
-        localStorage.clear(); 
         const mockStore = getMockStore({ ...notAuthenticatedState });   
         const { result } = renderHook( () => useAuthStore(), {
             wrapper: ({ children }) => <Provider store={ mockStore }>{ children }</Provider>
@@ -63,7 +64,6 @@ describe('Pruebas en el useAuthStore', () => {
     });
     
     test('startLogin debe de fallar la autenticación', async() => {
-        localStorage.clear();
         const mockStore = getMockStore({ ...notAuthenticatedState });   
         const { result } = renderHook( () => useAuthStore(), {
             wrapper: ({ children }) => <Provider store={ mockStore }>{ children }</Provider>
@@ -83,6 +83,40 @@ describe('Pruebas en el useAuthStore', () => {
         await waitFor(
             () => expect( result.current.errorMessage ).toBe( undefined )
         );
+    });
+
+    test('startRegister debe de crear un usuario', async() => {
+
+        const newUser = { email: 'algo@google.com', password: '123456789', name: 'Test User 2'};
+
+        const mockStore = getMockStore({ ...notAuthenticatedState });   
+        const { result } = renderHook( () => useAuthStore(), {
+            wrapper: ({ children }) => <Provider store={ mockStore }>{ children }</Provider>
+        });
+        
+        const spy = jest.spyOn( calendarApi, 'post' ).mockReturnValue({
+            data: {
+                ok: true,
+                uid: '123456789123',
+                name: 'Test User',
+                token: 'ALGUN-TOKEN'
+            }
+        });
+        
+
+        await act( async() => {
+            await result.current.startRegister( newUser );
+        });
+
+        const { errorMessage, status, user } = result.current;
+        
+        expect({ errorMessage, status, user }).toEqual({
+            errorMessage: undefined,
+            status: 'authenticated',
+            user: { name: 'Test User', uid: '123456789123' }
+        });
+
+        spy.mockRestore(); // esto es para destruir el spy 
     });
 
 });
