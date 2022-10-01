04fc25bd8558ff3cdfe5899dd95f497de9693bd2
Enoc Castro Vargas <enocco70@gmail.com>
2022-09-30 22:57:26 -0600

    Pruebas en el componente FabDelete

--- tests/calendar/components/FabDelete.test.jsx
+++ tests/calendar/components/FabDelete.test.jsx
@@ -1,20 +1,24 @@
 import { render, screen } from '@testing-library/react';
-import { Provider } from 'react-redux';
 import { FabDelete } from '../../../src/calendar/components/FabDelete';
-import { store } from '../../../src/store';
+import { useCalendarStore } from '../../../src/hooks/useCalendarStore';
 
+jest.mock('../../../src/hooks/useCalendarStore');
 
 describe('Pruebas en <FabDelete />', () => {
-    
+
     test('debe de mostrar el componente correctamente', () => {
 
-        render( 
-            <Provider store={ store }>
-                <FabDelete />
-            </Provider>
-        );
-        screen.debug();
-        
+        useCalendarStore.mockReturnValue({
+            hasEventSelected: false
+        });
+
+        render(<FabDelete />);
+
+        const btn = screen.getByLabelText('btn-delete');
+        expect( btn.classList ).toContain('btn');
+        expect( btn.classList ).toContain('btn-danger');
+        expect( btn.classList ).toContain('fab-danger');
+        expect( btn.style.display ).toBe('none');
     });
 
 });
